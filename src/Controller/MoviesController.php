f67f0cacd9e7ea230a933cfa931a3b9b3c417273
Virendra Patil <virendra.patil@hbwsl.com>
2025-03-18 12:39:20 +0530

    video 8

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -8,14 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}',defaults:['name'=>null], name: 'app_movies')]
-    public function index($name): Response
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Virendra,Welcome to the movies page',
-            'path' => 'src/Controller/MoviesController.php',
-            'name' => $name
-        ]);
+      return $this->render('index.html.twig',['Title'=>'Movies']);
         
     }
 }
