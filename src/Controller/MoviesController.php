bd9be81bec998508671449b1adc678ab2eda044c
Virendra Patil <virendra.patil@hbwsl.com>
2025-03-18 15:35:28 +0530

    video 14

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\MovieRepository;
+
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'movies')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        $movies=["Avengers","dark knight","joker"];
-      return $this->render('index.html.twig',array('movies'=>$movies));
-        
+        $movies = $movieRepository->findAll();
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }
 }
